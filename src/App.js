bfb6adf01056c8820b4d0f211faadef54b4911b1
Akhil Eaga <akhil.bitsian2013@gmail.com>
2023-11-14 20:07:41 +0530

    barebones setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello world</p>
+      <button class="btn btn-primary">Hello World</button>
+    </div>
+  );
+}
+
+export default App;
